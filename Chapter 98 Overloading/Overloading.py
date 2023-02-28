5e15acdaff954f2f709ac081af91a4c3f03afc46
apurvanimkar <nimkarapurva2000@gmail.com>
2023-02-28 17:13:05 +0530

    commit

---
+++ Chapter 98 Overloading/Overloading.py
@@ -0,0 +1,18 @@
+
+#Overloading
+
+class Myclass:
+    def sum(self,a=None,b=None,c=None):
+        if(a!=None and b!=None and c!=None):
+            s=a+b+c
+            
+        elif(a!=None and b!=None):
+            s=a*b
+        else:
+            s="Invalid"
+
+        return s
+     
+
+obj=Myclass()
+print(obj.sum(10,20))
